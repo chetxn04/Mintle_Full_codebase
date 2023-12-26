57448ff9e4057ee0fe1a31a69f3791c413817c99
isha-prabhu <ishaprabhu81@gmail.com>
2023-12-26 20:56:40 +0530

    landing screen 3

---
+++ android/app/src/main/kotlin/com/example/landing_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.landing_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LandingScreen(),
+    );
+  }
+}
+
+class LandingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.black,Color(0xFF000D39)],
+          ),
+        ),
+        child: Center(
+          child: Container(
+            // padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                //
+                Container(
+
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width*1.1,
+                  color: Colors.transparent,
+                  child: Image(
+                    image: AssetImage('images/img3.png'),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                //
+                Text(
+                  'Know where your',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                  ),
+                ),
+                SizedBox(height: 6.0),
+
+                Text(
+                  'money goes',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                //
+                Text(
+                  'Track your transactions easily,',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+                SizedBox(height: 6.0),
+
+                Text(
+                  'with categories',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                //
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _buildCircle(false),
+                    _buildCircle(false),
+                    _buildCircle(true),
+                  ],
+                ),
+                SizedBox(height: 25.0),
+                //
+                OutlinedButton(
+                  onPressed: () {
+
+                  },
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Color(0x3B65F9)),
+                    backgroundColor: Color(0xFF0032E4), // Set the background color to white
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    fixedSize: Size(300, 50), //
+                  ),
+                  child: Text(
+                    "Sign up",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                // f) Login Button
+                OutlinedButton(
+                  onPressed: () {
+
+                  },
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Color(0x3B65F9)),
+                    backgroundColor: Color(0xFFFFF3DC), // Set the background color to white
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    fixedSize: Size(300, 50), //
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Color(0xFF7F3DFF), fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCircle(bool isActive) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5.0),
+      width: 10.0,
+      height: 10.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: isActive ? Colors.white : Colors.grey,
+      ),
+    );
+  }
+}
